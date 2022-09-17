85c62eaf69516336a3ca68801e018536152c5041
Pedro <olivares.m02@mymail.sxu.edu>
2022-09-17 18:02:47 -0400

    Chapter 3 submit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const next = require('next');
-
-const mongoose = require('mongoose');
-
 const session = require('express-session');
 const mongoSessionStore = require('connect-mongo');
+const next = require('next');
+const mongoose = require('mongoose');
 
+const setupGoogle = require('./google');
 const User = require('./models/User');
 
 require('dotenv').config();
@@ -19,12 +18,8 @@ const options = {
   useFindAndModify: false,
   useUnifiedTopology: true,
 };
-mongoose.connect(MONGO_URL, options);
 
-// mongoose
-//   .connect(MONGO_URL, options)
-//   .then(() => console.log('Connected')) // eslint-disable-line no-console
-//   .catch((err) => console.log('Caught', err.stack)); // eslint-disable-line no-console
+mongoose.connect(MONGO_URL, options);
 
 const port = process.env.PORT || 8000;
 const ROOT_URL = `http://localhost:${port}`;
@@ -36,9 +31,8 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  // configuring MongoDB session store
+  // confuring MongoDB session store
   const MongoStore = mongoSessionStore(session);
-
   const sess = {
     name: process.env.SESSION_NAME,
     secret: process.env.SESSION_SECRET,
@@ -57,23 +51,19 @@ app.prepare().then(() => {
 
   server.use(session(sess));
 
-  // server.get('/', (req, res) => {
-  //   const user = { email: 'team@builderbook.org' };
-  //   app.render(req, res, '/', { user });
+  // server.get('/', async (req, res) => {
+  //   const user = await User.findOne({ slug: 'team-builder-book' });
+  //   req.user = user;
+  //   app.render(req, res, '/');
   // });
 
-  // this is testing code, remove later
-  server.get('/', async (req, res) => {
-    req.session.foo = 'bar';
-    const user = await User.findOne({ slug: 'team-builder-book' });
-    app.render(req, res, '/', { user });
-  });
+  setupGoogle({ server, ROOT_URL });
 
   server.get('*', (req, res) => handle(req, res));
 
   // starting express server
   server.listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on ${ROOT_URL}`);
+    console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
   });
 });
